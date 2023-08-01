76c7c6cd056782853eb20d693640eea08b854d2d
glabojko <glabojko@gmail.com>
2023-08-01 20:26:25 +0200

    atributes and instances skeleton added for ToDoItem.cs

--- ToDoItem.cs
+++ ToDoItem.cs
@@ -8,5 +8,45 @@ namespace Eisenhower_Matrix
 {
     internal class ToDoItem
     {
+        private string Title { get; set; }
+        private DateTime Deadline { get; set; }
+        private bool IsDone { get; set; } = false;
+
+        public void ToDoItem(string title, DateTime deadline)
+        {
+            Title = title;
+            Deadline = deadline;
+        }
+
+        public string GetTitle() 
+        {
+            string titleInput = Console.ReadLine();
+            return titleInput;
+
+        }
+
+
+        public string GetDeadline()
+        {
+            string deadlineInput = Console.ReadLine();
+            return deadlineInput;
+        }
+
+        public void Mark()
+        {
+            IsDone = true;
+        }
+
+        public void Unmark()
+        {
+            IsDone = false;
+        }
+
+        public string ToString()
+        {
+            return $"[{IsDone}] {Deadline} {Title}";
+        }
+
+
     }
 }
