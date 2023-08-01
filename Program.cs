8fa3b483c30d28e9216034097658b1c405542028
RadekRo <r.roclawski@ankara.pl>
2023-08-01 19:45:56 +0200

    Created basic repository with empty Readme File and gitignore

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Eisenhower_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
