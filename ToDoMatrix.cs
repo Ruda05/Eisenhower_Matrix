66abdb4b8f937b96656f783823ec50e7afb94dc7
RadekRo <r.roclawski@ankara.pl>
2023-08-20 17:00:27 +0200

    Formatted table like return in Matrix object modifying a ToString() method

--- ToDoMatrix.cs
+++ ToDoMatrix.cs
@@ -53,7 +53,38 @@ namespace Eisenhower_Matrix
         {
             //  Returns a todoQuarters list (an Eisenhower todoMatrix) formatted to string.
             // return $"[{IsDone}] {Deadline} {Title}";
-            return "Ale chujnia!";
+            string table = @"
+    |            URGENT              |           NOT URGENT           |
+  --|--------------------------------|--------------------------------|--
+    | 1. [ ] 9-6  go to the doctor   |                                |
+    | 2. [x] 11-6 submit assignment  |                                |
+  I |                                |                                |
+  M |                                |                                |
+  P |                                |                                |
+  O |                                |                                |
+  R |                                |                                |
+  T |                                |                                |
+  A |                                |                                |
+  N |                                |                                |
+  T |                                |                                |
+    |                                |                                |
+    |                                |                                |
+  --|--------------------------------|--------------------------------|--
+  N | 1. [ ] 14-6 buy a ticket       | 1. [x] 30-5 House of Cards     |
+  O |                                |                                |
+  T |                                |                                |
+    |                                |                                |
+  I |                                |                                |
+  P |                                |                                |
+  O |                                |                                |
+  R |                                |                                |
+  T |                                |                                |
+  A |                                |                                |
+  N |                                |                                |
+  T |                                |                                |
+  --|--------------------------------|--------------------------------|--
+";
+            return table;
         }
     }
 }
