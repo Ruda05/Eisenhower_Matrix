8e9b679c7dca3a46510fbf101e7663aec421c056
RadekRo <r.roclawski@ankara.pl>
2023-08-26 13:03:51 +0200

    Continued refactoring code

--- Program.cs
+++ Program.cs
@@ -2,8 +2,6 @@
 using Microsoft.VisualBasic;
 using System.Globalization;
 
-
-Console.WriteLine("Hello, Eisenhower!");
 var input = new Input();
 var display = new Display();
 ToDoMatrix toDoMatrix = new ToDoMatrix();
@@ -17,8 +15,8 @@ while (isActive)
     var deadline = input.GetDeadline();
     display.DisplayQuestion("Is your task important? (Y/N)");
     string importanceStatusINPUT = input.GetImportanceStatus();
-    display.DisplayQuestion("Is this task already done? Y/N: ");
-    Console.WriteLine($"Your task is already done:{input.IsDone(input.GetStatus())}");
+    //display.DisplayQuestion("Is this task already done? Y/N: ");
+    //Console.WriteLine($"Your task is already done:{input.IsDone(input.GetStatus())}");
     toDoMatrix.AddItem(userInputTitle, deadline, importanceStatusINPUT == "important");
 
     display.DisplayQuestion("Do you want to add next task? [Y/N]");
