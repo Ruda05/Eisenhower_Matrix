90236db710f688f1d64f4d8e3de8ecbc6ab5e03a
RadekRo <r.roclawski@ankara.pl>
2023-08-27 10:17:02 +0200

    Changed loop structure for main menu options

--- Program.cs
+++ Program.cs
@@ -2,26 +2,56 @@
 using Microsoft.VisualBasic;
 using System.Globalization;
 
-var input = new Input();
-var display = new Display();
-ToDoMatrix toDoMatrix = new ToDoMatrix();
-
-bool isActive = true;
-while (isActive)
+namespace Eisenhower_Matrix
 {
-    Console.Clear();
-    display.DisplayQuestion("Input task title: ");
-    string userInputTitle = input.GetTitle();
-    display.DisplayQuestion("Input deadline in format DD-MM: ");
-    var deadline = input.GetDeadline();
-    display.DisplayQuestion("Is your task important? (Y/N)");
-    string importanceStatusInput = input.GetImportanceStatus();
-    bool isImportant = input.IsImportant(importanceStatusInput);
-    toDoMatrix.AddItem(userInputTitle, deadline, isImportant);
-    display.DisplayQuestion("Do you want to add next task? [Y/N]");
-    string nextTask = Console.ReadLine();
-    isActive = nextTask == "Y";
-}
 
-Console.Clear();
-Console.WriteLine(toDoMatrix.ToString());
+    internal class Program
+    {
+        static void Main()
+        {
+            var input = new Input();
+            var display = new Display();
+            ToDoMatrix toDoMatrix = new ToDoMatrix();
+            string currentOption = "T";
+
+            bool isActive = true;
+            while (isActive)
+            {
+                if (currentOption == "T")
+                {
+                    Console.Clear();
+                    Console.WriteLine(toDoMatrix.ToString());
+                    display.DisplayQuestion("Select an option:\n[A] Add\n[Q] Quit\nYour choice: ");
+                    currentOption = Console.ReadLine();
+                    if (currentOption == "Q") 
+                    { 
+                        isActive = false;
+                    }
+                }
+                else if (currentOption == "A")
+                {
+                    Console.Clear();
+                    display.DisplayQuestion("Input task title: ");
+                    string userInputTitle = input.GetTitle();
+                    display.DisplayQuestion("Input deadline in format DD-MM: ");
+                    var deadline = input.GetDeadline();
+                    display.DisplayQuestion("Is your task important? (Y/N)");
+                    string importanceStatusInput = input.GetImportanceStatus();
+                    bool isImportant = input.IsImportant(importanceStatusInput);
+                    toDoMatrix.AddItem(userInputTitle, deadline, isImportant);
+                    display.DisplayQuestion("Do you want to add next task? [Y/N]");
+                    string nextTask = Console.ReadLine();
+                    if (nextTask == "Y")
+                    {
+                        currentOption = "A";
+                    }
+                    else
+                    {
+                        currentOption = "T";
+                    }
+
+                }
+            }
+        }
+    }
+}
