d453e9bf8c251b1e052d006fc689e3c1014e5a2f
RadekRo <r.roclawski@ankara.pl>
2023-08-28 15:48:52 +0200

    Created Scripts directory to keep an sql query for proper table creation in matrix db

---
+++ Scripts/CreateTable.sql
@@ -0,0 +1,9 @@
+﻿DROP TABLE IF EXISTS todoitems;
+
+CREATE TABLE todoitems (
+    id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    deadline DATE,
+    important BIT,
+    completed BIT DEFAULT 0
+);
