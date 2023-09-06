33741fc1fb0dad5be649316bf6f185f6f27009d0
RadekRo <r.roclawski@ankara.pl>
2023-09-06 21:43:41 +0200

    Reading from db and shuffle to proper Quarter done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Eisenhower_Matrix.Manager;
 using Eisenhower_Matrix.Model;
+using Eisenhower_Matrix.View;
 
 namespace Eisenhower_Matrix
 {
@@ -13,6 +14,12 @@ namespace Eisenhower_Matrix
             var manager = new MatrixDbManager();
 
             Console.WriteLine(manager.TestConnection());
+            ToDoMatrix toDoMatrix = new ToDoMatrix();
+            var input = new Input();
+            var display = new Display();
+            string currentOption = "T";
+
+            /*
             var testItem = new ToDoItem("greg", DateTime.Now.AddDays(5));
             Console.WriteLine(testItem.IsDone);
             testItem.Mark();
@@ -22,93 +29,88 @@ namespace Eisenhower_Matrix
             testItem.MakeImportant();
             Console.WriteLine(testItem.IsImportant);
             Console.WriteLine(testItem.Id);
-            //testItem.Id = 1;
-            //manager.UpdateItem(testItem);
-            //Add item - adding item to the database - Test
-            //manager.AddItem(testItem);
+            testItem.Id = 1;
+            manager.UpdateItem(testItem);
+            Add item - adding item to the database - Test
+            manager.AddItem(testItem);
+            */
             var userList = manager.GetAllItems();
             foreach (var item in userList)
             {
                 Console.WriteLine(item);
                 Console.WriteLine(item.IsImportant);
                 Console.WriteLine(item.IsDone);
+                toDoMatrix.AddItem(item.Title, item.Deadline, item.IsImportant);
             }
 
+            bool isActive = true;
+            while (isActive)
+            {
+                if (currentOption == "T")
+                {
+                    Console.Clear();
+                    Console.WriteLine(toDoMatrix.ToString());
+                    display.DisplayQuestion("Select an option:\n[A]dd\n[D]elete\n[Q]uit\nYour choice: ");
+                    currentOption = Console.ReadLine();
+                    if (currentOption == "Q")
+                    {
+                        isActive = false;
+                    }
+                }
+                else if (currentOption == "A")
+                {
+                    Console.Clear();
+                    display.DisplayQuestion("Input task title: ");
+                    string userInputTitle = input.GetTitle();
+                    display.DisplayQuestion("Input deadline in format DD-MM: ");
+                    var deadline = input.GetDeadline();
+                    display.DisplayQuestion("Is your task important? (Y/N)");
+                    string importanceStatusInput = input.GetImportanceStatus();
+                    bool isImportant = input.IsImportant(importanceStatusInput);
+                    toDoMatrix.AddItem(userInputTitle, deadline, isImportant);
+                    display.DisplayQuestion("Do you want to add next task? [Y/N]");
+                    string nextTask = Console.ReadLine();
+                    if (nextTask == "Y")
+                    {
+                        currentOption = "A";
+                    }
+                    else
+                    {
+                        currentOption = "T";
+                    }
 
+                }
+                else if (currentOption == "D")
+                {
+                    display.DisplayQuestion("Which task to delete?\nSelect group [1-4]:");
+                    string groupToDelete = Console.ReadLine();
+                    string groupMark;
+                    switch (groupToDelete)
+                    {
+                        case "1":
+                            groupMark = "IU";
+                            break;
+                        case "2":
+                            groupMark = "IN";
+                            break;
+                        case "3":
+                            groupMark = "NU";
+                            break;
+                        case "4":
+                            groupMark = "NN";
+                            break;
+                        default:
+                            groupMark = " ";
+                            break;
+                    }
+                    display.DisplayQuestion("Select task number:");
+                    string taskToDelete = Console.ReadLine();
 
-            //var input = new Input();
-            //var display = new Display();
-            //ToDoMatrix toDoMatrix = new ToDoMatrix();
-            //string currentOption = "T";
-
-            //bool isActive = true;
-            //while (isActive)
-            //{
-            //    if (currentOption == "T")
-            //    {
-            //        Console.Clear();
-            //        Console.WriteLine(toDoMatrix.ToString());
-            //        display.DisplayQuestion("Select an option:\n[A]dd\n[D]elete\n[Q]uit\nYour choice: ");
-            //        currentOption = Console.ReadLine();
-            //        if (currentOption == "Q") 
-            //        { 
-            //            isActive = false;
-            //        }
-            //    }
-            //    else if (currentOption == "A")
-            //    {
-            //        Console.Clear();
-            //        display.DisplayQuestion("Input task title: ");
-            //        string userInputTitle = input.GetTitle();
-            //        display.DisplayQuestion("Input deadline in format DD-MM: ");
-            //        var deadline = input.GetDeadline();
-            //        display.DisplayQuestion("Is your task important? (Y/N)");
-            //        string importanceStatusInput = input.GetImportanceStatus();
-            //        bool isImportant = input.IsImportant(importanceStatusInput);
-            //        toDoMatrix.AddItem(userInputTitle, deadline, isImportant);
-            //        display.DisplayQuestion("Do you want to add next task? [Y/N]");
-            //        string nextTask = Console.ReadLine();
-            //        if (nextTask == "Y")
-            //        {
-            //            currentOption = "A";
-            //        }
-            //        else
-            //        {
-            //            currentOption = "T";
-            //        }
-
-            //    }
-            //    else if (currentOption == "D") 
-            //    {
-            //        display.DisplayQuestion("Which task to delete?\nSelect group [1-4]:");
-            //        string groupToDelete = Console.ReadLine();
-            //        string groupMark;
-            //        switch (groupToDelete)
-            //        {
-            //            case "1":
-            //                groupMark = "IU";
-            //                break;
-            //            case "2":
-            //                groupMark = "IN";
-            //                break;
-            //            case "3":
-            //                groupMark = "NU";
-            //                break;
-            //            case "4":
-            //                groupMark = "NN";
-            //                break;
-            //            default:
-            //                groupMark = " ";
-            //                break;
-            //        }
-            //        display.DisplayQuestion("Select task number:");
-            //        string taskToDelete = Console.ReadLine();
-
-            //        currentOption = "T";
-            //    }
-            //}
-            //Console.Clear();
-            //display.DisplayQuestion("Thank you for using our Application.\nSee you soon!\n\nCreated (2023) by:\nRadosław Rocławski\nGrzegorz Łabojko\n\n\n\n");
+                    currentOption = "T";
+                }
+            }
+            Console.Clear();
+            display.DisplayQuestion("Thank you for using our Application.\nSee you soon!\n\nCreated (2023) by:\nRadosław Rocławski\nGrzegorz Łabojko\n\n\n\n");
         }
     }
 }
