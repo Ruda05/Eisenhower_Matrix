81d6317cef184e6a3c7782c527fc23e6ae61b6f9
RadekRo <r.roclawski@ankara.pl>
2023-09-08 22:04:06 +0200

    Changed default value

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace Eisenhower_Matrix
 
     internal class Program
     {
-        public static string SelectedQuarter { get; private set; } = "NU";
-        public static int SelectedTask { get; private set; } = 3;
+        public static string SelectedQuarter { get; private set; } = "IU";
+        public static int SelectedTask { get; private set; } = 0;
         public static void Main(string[] args)
         {
            
